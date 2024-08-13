9c377c0763d126f241b2ad707f682689a9e628a0
Purbai <purbai.varsani@softwire.com>
2024-08-13 11:28:19 +0100

    initial repo setup

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Bookish.Models;
+
+namespace Bookish.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        Console.WriteLine("inside HomeController");
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+                Console.WriteLine("inside IActionResult Index");
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        Console.WriteLine("inside IActionResult Privacy");
+        return View();
+    }
+
+    public IActionResult Librarian()
+    {
+        Console.WriteLine("inside IActionResult Librarian");
+        return View(new LibrarianViewModel());
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/LibrarianViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bookish.Models;
+
+public class LibrarianViewModel
+{
+    public static int lastId = 0;
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+}
